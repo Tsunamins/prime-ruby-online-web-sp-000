94aa75597fd95fd6a0d9de32cd9076f0ebfc0782
Alison Reilly <reillyamr@gmail.com>
2018-12-07 20:15:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(value)
+  root_value = Integer.sqrt(value)
+  
+  counter = 2 
+  while counter = root_value do 
+    i % counter 
+    counter += 1 
+  end 
+  if i % counter == 0 
+    return false 
+  else 
+    return true 
+    
+  end 
+end 
+    
